f046ee0e666c4af9d46f9c0879ea755150678a92
Satabrata Paul <satabrata.slg88@gmail.com>
2022-02-19 14:53:28 +0530

    Apollo Client (React) scractchboard created

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import { Component } from "react";
+import BookList from "./components/BookList.js";
+import {ApolloClient,InMemoryCache,ApolloProvider} from "@apollo/client";
+
+//Creating Apollo Client 
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache()
+});
+
+
+class App extends Component{
+  render(){
+    return (
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Reading Book List</h1>
+          <BookList/>
+        </div>
+      </ApolloProvider>
+  );
+}
+}
+
+export default App;
+
