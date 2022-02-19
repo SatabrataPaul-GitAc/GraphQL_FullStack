14aa28669dcbde51bc3f5839e757d896dedadbbf
Satabrata Paul <satabrata.slg88@gmail.com>
2022-02-19 23:24:05 +0530

    React jsx booklist rendered

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,20 +1,35 @@
-import {gql,useQuery} from "@apollo/client";
+import {enableExperimentalFragmentVariables, gql,useQuery} from "@apollo/client";
 
 const getBooksQuery = gql`
 {
     books{
+        id
         name
         genre
     }
 }
 `
 
+function DisplayBooks(){
+    const {data,loading} = useQuery(getBooksQuery);
+
+    if(loading===true) return <div>Loading Books ....</div>
+
+    else if(loading===false){
+        return (
+            data.books.map((book)=>{
+                return <li key={book.id}>{book.name}</li>
+            })
+        )
+    }
+}
+
 function BookList(){
-    const{data} = useQuery(getBooksQuery);
+
     return (
         <div>
                 <ul id="book-list">
-                    <li>Book name</li>
+                    {DisplayBooks()}
                 </ul>
             </div>
     )
