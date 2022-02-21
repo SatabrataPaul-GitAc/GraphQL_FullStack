4f0fbb01bc3db82c9e226d1c6c2f4031546ed2ce
Satabrata Paul <satabrata.slg88@gmail.com>
2022-02-21 14:00:25 +0530

    Configured React Component State

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,11 +1,56 @@
 import { useQuery } from "@apollo/client";
+import { useReducer} from "react";
 import { getAuthorsQuery } from "../queries/queries";
 
+const initialstate = {
+    name: "",
+    genre: "",
+    authorid: ""
+}
+
+const reducer = (state,action)=>{
+    switch(action.type){
+        case "bookname":
+            state.name=action.bookname;
+            return state;
+        case "genre":
+            state.genre=action.genre;
+            return state;
+        case "authorid":
+            state.authorid=action.authorid;
+            return state;
+        default:
+            throw new Error();    
+    }
+}
+
+const bookName = (e) =>{
+    return (
+        {type: "bookname",bookname: e.target.value}
+    )
+}
+
+const genre = (e)=>{
+    return (
+        {type: "genre",genre: e.target.value}
+    )
+}
+
+const authorid = (e)=>{
+    return (
+        {type: "authorid",authorid: e.target.value}
+    )
+}
+
+function submitForm(e){
+    e.preventDefault();
+    console.log(initialstate);
+}
 
 function GetAuthors(){
     const {data,loading} = useQuery(getAuthorsQuery);
 
-    if(loading===true) return <option>Loading Authors ...</option>
+    if(loading===true) return <option>Loading authors ...</option>
 
     else if(loading===false){
         return (
@@ -18,24 +63,30 @@ function GetAuthors(){
 
 
 function AddBook(){
+
+    const [state,dispatch] = useReducer(reducer, initialstate);
+
     return (
-        <form id="add-book">
+        <form id="add-book" onSubmit={(e)=>submitForm(e)}>
             <div className="field">
                 <label>BookName : </label>
-                <input type="text" />
+                <input type="text" onChange={(e)=>{dispatch(bookName(e))}}></input>
             </div>
 
             <div className="field">
                 <label>Genre : </label>
-                <input type="text" />
+                <input type="text" onChange={(e)=>{dispatch(genre(e))}}/>
             </div>
 
             <div className="field">
                 <label>Author : </label>
-                <select>
-                    <option>{GetAuthors()}</option>
+                <select onChange={(e)=>{dispatch(authorid(e))}}>
+                    <option>Select Author</option>
+                    {GetAuthors()}
                 </select>
             </div>
+
+            <button>+</button>
         </form>
     )
 }
