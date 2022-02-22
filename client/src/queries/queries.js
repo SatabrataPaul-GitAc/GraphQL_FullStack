3818eb17e42796910f2ab6368b215c54767a4c0e
Satabrata Paul <satabrata.slg88@gmail.com>
2022-02-22 15:20:00 +0530

    update mutation query

--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -18,4 +18,13 @@ const getAuthorsQuery = gql`
     }
 }`
 
-export {getBooksQuery,getAuthorsQuery};
+
+const addBookMutation = gql`
+mutation($name: String!,$genre: String!,$author_id: ID!){
+    addBook(name:$name, genre:$genre, author_id:$author_id){
+        name
+        id
+    }
+}`
+
+export {getBooksQuery,getAuthorsQuery,addBookMutation};
