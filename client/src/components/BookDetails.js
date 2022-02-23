b50b6e723f588f6d79ff6518ab93c81b0e960dea
Satabrata Paul <satabrata.slg88@gmail.com>
2022-02-24 00:34:44 +0530

    Update React BookDetails Component

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -13,14 +13,22 @@ function BookDetails(props){
         if(data!==undefined){
             return (
                 <div>
-                    <p>BookName : {data.book.name}</p>
+                    <h2>BookName : {data.book.name}</h2>
                     <p>Genre: {data.book.genre}</p>
                     <p>Author: {data.book.author.name}</p>
+                    <p>All Books by this Author: </p>
+                    <ul className="other-books">
+                        {data.book.author.books.map((item)=>{
+                            return (
+                                <li key={item.id}>{item.name}</li>
+                            )
+                        })}
+                    </ul>
                 </div>
             )
         }
         else{
-            return null
+            return <p>No Book Selected...</p>
         }
     }
 }
