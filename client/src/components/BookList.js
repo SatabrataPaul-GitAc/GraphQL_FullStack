dfb36313c353552b92868c07030bb40604286bf0
Satabrata Paul <satabrata.slg88@gmail.com>
2022-02-23 18:22:05 +0530

    Add Component DisplayBooks

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,28 +1,14 @@
-import {useQuery} from "@apollo/client";
-import {getBooksQuery} from "../queries/queries";
-
-function DisplayBooks(){
-    const {data,loading} = useQuery(getBooksQuery);
-
-    if(loading===true) return <div>Loading Books ....</div>
-
-    else if(loading===false){
-        return (
-            data.books.map((book)=>{
-                return <li key={book.id}>{book.name}</li>
-            })
-        )
-    }
-}
+import {DisplayBooks} from "./DisplayBooks";
 
 function BookList(){
 
     return (
         <div>
                 <ul id="book-list">
-                    {DisplayBooks()}
+                    <DisplayBooks/>
                 </ul>
-            </div>
+                
+        </div>
     )
 }
 
