e43b9c62b509f6faeedc1b1f2c6e3bd37094d582
Satabrata Paul <satabrata.slg88@gmail.com>
2022-02-23 18:31:04 +0530

    Configured DisplayBooks React Component

---
+++ client/src/components/DisplayBooks.js
@@ -0,0 +1,30 @@
+import {useQuery} from "@apollo/client";
+import { useState } from "react";
+import {getBooksQuery} from "../queries/queries";
+import BookDetails from "./BookDetails";
+
+
+function DisplayBooks(){
+    const [selected,setState] = useState("");
+    const {data,loading} = useQuery(getBooksQuery);
+
+    if(loading===true) return <div>Loading Books ....</div>
+
+    else if(loading===false){
+        return (
+            <div>
+                {data.books.map((book)=>{
+                    return <li key={book.id} onClick={(e)=>{
+                        setState(book.id);
+                    }}>{book.name}</li>
+                })}
+                <BookDetails bookid={selected}/>
+            </div>
+        )
+    }
+    
+}
+
+
+
+export {DisplayBooks};
